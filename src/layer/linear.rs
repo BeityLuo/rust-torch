99a2cae8c17bc8a551eedc9cee135c1b2ec81c9c
beityluo <beityluo@163.com>
2025-01-02 17:53:24 +0800

    modify matmul3 function to MatMul3 struct, to automatically do backward propagada

--- src/layer/linear.rs
+++ src/layer/linear.rs
@@ -53,13 +53,13 @@ impl Linear {
         return x.dot(&self.w) + &self.b;
     }
 
-    pub fn backward(&mut self, output_grad: &ArrayView2<f64>) -> Array2<f64>{
-        let batch_size = output_grad.shape()[0];
+    pub fn backward(&mut self, grad: &ArrayView2<f64>) -> Array2<f64>{
+        let batch_size = grad.shape()[0];
         
         self.w_grad = Some(self.input.as_ref().unwrap().t()
-                      .dot(output_grad) / batch_size as f64);
-        self.b_grad = Some(output_grad.sum_axis(Axis(0)));
-        return output_grad.dot(&self.w.t()) / self.w.shape()[1] as f64;
+                      .dot(grad) / batch_size as f64);
+        self.b_grad = Some(grad.sum_axis(Axis(0)));
+        return grad.dot(&self.w.t()) / self.w.shape()[1] as f64;
     }
 
     pub fn step(&mut self, lr: f64) {
@@ -82,17 +82,70 @@ impl Scale {
 }
 
 pub struct MatMul3 {
-    input: Option<Array3<f64>>
+    a: Option<Array3<f64>>,
+    b: Option<Array3<f64>>,
+}
+
+impl MatMul3 {
+    pub fn init() -> Self { MatMul3 { a: None, b: None } }
+
+    pub fn forward(&mut self, a: &ArrayView3<f64>, b: &ArrayView3<f64>)
+        -> Array3<f64> {
+        // a: (batch, d1, d2), b: (batch, d3, d2)
+        assert_eq!(a.shape()[0], b.shape()[0]);
+        assert_eq!(a.shape()[1], b.shape()[2]);
+
+        self.a = Some(a.to_owned());
+        self.b = Some(b.to_owned());
+
+        let mut ret = Array3::zeros((a.shape()[0], a.shape()[1], b.shape()[1]));
+        for i in 0..a.shape()[0] {
+            ret.slice_mut(s![i,..,..])
+               .assign(&a.slice(s![i,..,..])
+                         .dot(&b.slice(s![i,..,..]).t()));
+        }
+    
+        return ret;
+    }
+
+    pub fn backward(&mut self, grad: &ArrayView3<f64>)
+        -> (Array3<f64>, Array3<f64>) {
+        // grad: (batch, d1, d3)
+        let a = self.a.as_ref().unwrap();
+        let b = self.b.as_ref().unwrap();
+        assert_eq!(a.shape()[0], grad.shape()[0]);
+        assert_eq!(a.shape()[1], grad.shape()[1]);
+        assert_eq!(b.shape()[1], grad.shape()[2]);
+
+        let mut a_ret = Array3::zeros((a.shape()[0], a.shape()[1], a.shape()[2]));
+        let mut b_ret = Array3::zeros((b.shape()[0], b.shape()[1], b.shape()[2]));
+        for i in 0..grad.shape()[0] {
+            a_ret.slice_mut(s![i,..,..])
+                 .assign(&grad.slice(s![i,..,..])
+                              .dot(&b.slice(s![i,..,..])));
+            b_ret.slice_mut(s![i,..,..])
+                 .assign(&grad.slice(s![i,..,..])
+                              .t()
+                              .dot(&a.slice(s![i,..,..])))
+        }
+
+        return (a_ret, b_ret);
+    }
 }
 
 pub struct ScaledDotProductAttention {
     soft: Sigmoid<Ix3>,
+    matmul3_1: MatMul3,
+    matmul3_2: MatMul3,
 }
 
 impl ScaledDotProductAttention {
     fn init(in_dim: usize, out_dim: usize) -> Self where Self: Sized {
         return ScaledDotProductAttention { 
-            soft: Sigmoid::<Ix3>::init(in_dim, out_dim) }
+            soft: Sigmoid::<Ix3>::init(in_dim, out_dim),
+            matmul3_1: MatMul3::init(),
+            matmul3_2: MatMul3::init(),
+        }
     }
 
 
@@ -102,14 +155,14 @@ impl ScaledDotProductAttention {
         assert_eq!(q.shape(), k.shape());
         assert_eq!(q.shape(), v.shape());
 
-        let mut temp = matmul3(&q.view(), &k.view());
+        let mut temp = self.matmul3_1.forward(&q.view(), &k.view());
         temp = Scale::forward(&temp.view(), (d as f64).sqrt());
         temp = self.soft.forward(&temp.view());
-        temp = matmul3(&temp.view(), v);
+        temp = self.matmul3_2.forward(&temp.view(), v);
         return temp;
     }
 
-    fn backward(&mut self, output_grad: &ArrayView2<f64>) -> Array2<f64> {
+    fn backward(&mut self, grad: &ArrayView2<f64>) -> Array2<f64> {
         todo!()
     }
 
